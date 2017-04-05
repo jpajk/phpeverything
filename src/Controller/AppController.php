7c7ed394c6bd69d3cf9af45183a31e1a5a4fd909
shikohin <jpajak@yandex.com>
2017-04-05 19:59:20 +0200

    Add users and posts. Add authentication.

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -43,13 +43,16 @@ class AppController extends Controller
 
         $this->loadComponent('RequestHandler');
         $this->loadComponent('Flash');
+        $this->loadComponent('Auth', [
+            'loginRedirect' => [
+                'controller' => 'Posts',
+                'action' => 'index'
+            ],
+        ]);
 
-        /*
-         * Enable the following components for recommended CakePHP security settings.
-         * see http://book.cakephp.org/3.0/en/controllers/components/security.html
-         */
-        //$this->loadComponent('Security');
-        //$this->loadComponent('Csrf');
+
+        $this->loadComponent('Security');
+        $this->loadComponent('Csrf');
     }
 
     /**
