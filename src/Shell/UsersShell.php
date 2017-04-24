8a2daddb7b9dedd6d09adc9e598940e3cf5da515
shikohin <jpajak@yandex.com>
2017-04-24 20:13:56 +0200

    Add a simple shell for adding new users.

---
+++ src/Shell/UsersShell.php
@@ -0,0 +1,46 @@
+<?php
+namespace App\Shell;
+
+use Cake\Console\Shell;
+use App\Shell\Task\AddUserTask;
+
+/**
+ * Users shell command.
+ */
+class UsersShell extends Shell
+{
+    public $tasks = ['AddUser'];
+
+    /**
+     * Manage the available sub-commands along with their arguments and help
+     *
+     * @see http://book.cakephp.org/3.0/en/console-and-shells.html#configuring-options-and-generating-help
+     *
+     * @return \Cake\Console\ConsoleOptionParser
+     */
+    public function getOptionParser()
+    {
+        $parser = parent::getOptionParser();
+
+        return $parser;
+    }
+
+    /**
+     * main() method.
+     *
+     * @return bool|int|null Success or error code.
+     */
+    public function main()
+    {
+
+        if (in_array('add', $this->args)) {
+            $this->AddUser->main();
+            $this->stopExecution();
+        }
+    }
+
+    protected function stopExecution()
+    {
+        exit;
+    }
+}
---
+++ tests/TestCase/Shell/UsersShellTest.php
@@ -0,0 +1,70 @@
+<?php
+namespace App\Test\TestCase\Shell;
+
+use App\Shell\UsersShell;
+use Cake\TestSuite\TestCase;
+
+/**
+ * App\Shell\UsersShell Test Case
+ */
+class UsersShellTest extends TestCase
+{
+
+    /**
+     * ConsoleIo mock
+     *
+     * @var \Cake\Console\ConsoleIo|\PHPUnit_Framework_MockObject_MockObject
+     */
+    public $io;
+
+    /**
+     * Test subject
+     *
+     * @var \App\Shell\UsersShell
+     */
+    public $Users;
+
+    /**
+     * setUp method
+     *
+     * @return void
+     */
+    public function setUp()
+    {
+        parent::setUp();
+        $this->io = $this->getMockBuilder('Cake\Console\ConsoleIo')->getMock();
+        $this->Users = new UsersShell($this->io);
+    }
+
+    /**
+     * tearDown method
+     *
+     * @return void
+     */
+    public function tearDown()
+    {
+        unset($this->Users);
+
+        parent::tearDown();
+    }
+
+    /**
+     * Test getOptionParser method
+     *
+     * @return void
+     */
+    public function testGetOptionParser()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+
+    /**
+     * Test main method
+     *
+     * @return void
+     */
+    public function testMain()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+}
